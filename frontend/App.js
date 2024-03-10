433719e8cf320f2646c34edcd783347618692bbf
Alexander Galev <alex.galev@yahoo.com>
2024-03-09 19:59:13 -0800

    Implemented NativeWind in lieu of react-native-paper

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // some test tailwind classes
+    <View className='bg-green-700 flex h-screen justify-center'>
+      <Text className='text-red-700 text-2xl font-bold self-center'>
+        Tailwind in React Native
+      </Text>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,12 +1,7 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel'],
-      },
-    },
-  };
-};
-
+    plugins: ['nativewind/babel'],
+  }
+}
