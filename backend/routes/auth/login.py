0a83819075186bf8d7531a929b79215cf3c8d131
Isaac Song <isaacsongs10@gmail.com>
2024-03-12 15:12:15 -0700

    include title for import section

--- backend/routes/auth/login.py
+++ backend/routes/auth/login.py
@@ -1,4 +1,5 @@
-# Imported from __init__.py
-from .. import make_response, request, session, Resource
-# Uncomment line below once user model is committed
+# IMPORT STATEMENTS
+from .. import make_response, request, session, Resource # Imported from __init__.py
+# Uncomment line below once user model is committed and get rid of this line
 # from models.users import User
+from app_setup import db
