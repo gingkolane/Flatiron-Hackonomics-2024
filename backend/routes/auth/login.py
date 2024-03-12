55343d472eb17c718669c081fba75cac7adfb100
Isaac Song <isaacsongs10@gmail.com>
2024-03-12 15:15:05 -0700

    import flask jwt extended functions

--- backend/routes/auth/login.py
+++ backend/routes/auth/login.py
@@ -3,3 +3,9 @@ from .. import make_response, request, session, Resource # Imported from __init_
 # Uncomment line below once user model is committed and get rid of this line
 # from models.users import User
 from app_setup import db
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    set_access_cookies,
+    set_refresh_cookies
+)
