91d1957f6f4e5f8631f48f6970942c4a09236777
Isaac Song <isaacsongs10@gmail.com>
2024-03-12 16:06:15 -0700

    remove comment and import Alexs user model

--- backend/routes/auth/login.py
+++ backend/routes/auth/login.py
@@ -1,7 +1,6 @@
 # IMPORT STATEMENTS
 from .. import make_response, request, session, Resource # Imported from __init__.py
-# Uncomment line below once user model is committed and get rid of this line
-# from models.users import User
+from models.users import User
 from app_setup import db
 from flask_jwt_extended import (
     create_access_token,
