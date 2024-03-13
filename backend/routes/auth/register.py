7caf6ac888fb743d1e2966a694fde2d5629688f1
Isaac Song <isaacsongs10@gmail.com>
2024-03-13 15:50:03 -0700

    add import statements

---
+++ backend/routes/auth/register.py
@@ -0,0 +1,10 @@
+# REGISTER/SIGNUP ROUTE
+from .. import make_response, request, session, Resource
+from flask import jsonify
+from app_setup import db
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    set_access_cookies,
+    set_refresh_cookies
+)
