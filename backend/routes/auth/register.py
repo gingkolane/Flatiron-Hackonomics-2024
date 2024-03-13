7523133c9560154e5af48103a056fd9259bdc917
Isaac Song <isaacsongs10@gmail.com>
2024-03-13 15:59:39 -0700

    prevent duplicate emails

--- backend/routes/auth/register.py
+++ backend/routes/auth/register.py
@@ -1,6 +1,7 @@
 # REGISTER/SIGNUP ROUTE
 from .. import make_response, request, session, Resource
 from flask import jsonify
+from models.users import User
 from app_setup import db
 from flask_jwt_extended import (
     create_access_token,
@@ -12,6 +13,7 @@ from flask_jwt_extended import (
 class Register(Resource):
     def post(self):
         if (email := request.json.get('email')) and (password := request.json.get('password')):
-            pass
+            if (User.query.filter_by(email=email).first()):
+                return {'error': 'Email already exists'}, 409 
         else:
             return {'error': 'The server was unable to retrieve your information'}, 400
