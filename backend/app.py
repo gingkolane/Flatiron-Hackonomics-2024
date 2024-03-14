69bb4bac6137b2e8a25f7eb0b3ceaa8976688cf5
Isaac Song <isaacsongs10@gmail.com>
2024-03-14 15:20:54 -0700

    add jwt user lookup loader

--- backend/app.py
+++ backend/app.py
@@ -19,3 +19,13 @@ from routes.auth.register import Register
 #! Authentication
 api.add_resource(Login, '/login')
 api.add_resource(Register, '/register')
+
+
+# Register a callback function that loads a user from your database whenever 
+# a protected route is accessed. This should return any python object on a 
+# successful lookup, or None if the lookup failed for any reason
+# (For ex: if a user has been deleted from the database)
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data['sub']
+    return db.session.get(User, identity)
