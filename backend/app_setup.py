0fc7a7263491cd6e2f9c7c4071f9698543a7eba4
Isaac Song <isaacsongs10@gmail.com>
2024-03-14 15:37:26 -0700

    input jwt into app_setup

--- backend/app_setup.py
+++ backend/app_setup.py
@@ -8,6 +8,7 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from datetime import timedelta
 import os
 
@@ -38,14 +39,13 @@ metadata = MetaData(naming_convention={
     "ck": "ck_%(table_name)s_%(constraint_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"})
-db = SQLAlchemy(metadata=metadata)
+#! flask-sqlalchemy setup
+db = SQLAlchemy(app)
+#! flask-migrate setup
 migrate = Migrate(app, db)
-db.init_app(app)
-
-# Instantiate REST API
-api = Api(app)
-
-# Instantiate CORS
-CORS(app)
+#! flask-bcrypt
 bcrypt = Bcrypt(app)
- 
+#! flask-restful setup
+api = Api(app)
+#! flask-jwt-extended setup
+jwt = JWTManager(app)
