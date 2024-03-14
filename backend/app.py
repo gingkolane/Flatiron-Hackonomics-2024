edb1f1c580bd02e5274665d4b835d00cdb4b3064
Isaac Song <isaacsongs10@gmail.com>
2024-03-14 15:19:25 -0700

    add login/register route imports

--- backend/app.py
+++ backend/app.py
@@ -2,3 +2,14 @@
 
 # Local imports
 from app_setup import app, db, api, jwt
+
+# Remote library imports
+from flask_restful import Resource
+
+# Model imports
+from models.users import User
+
+# Route imports
+#! Authentication
+from routes.auth.login import Login
+from routes.auth.register import Register
