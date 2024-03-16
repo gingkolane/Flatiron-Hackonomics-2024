f33d90ded3562af13f654844455b1fe37bd6fc04
Isaac Song <isaacsongs10@gmail.com>
2024-03-15 17:52:26 -0700

    uncomment accounts relation and shift transaction import in accounts.py

--- backend/models/accounts.py
+++ backend/models/accounts.py
@@ -14,5 +14,5 @@ class Account(db.Model, SerializerMixin):
 
     user = db.relationship('User', back_populates='accounts')
     transactions = db.relationship('Transaction', back_populates='account')
-
-from models.transactions import Transaction
+    
+    from models.transactions import Transaction
