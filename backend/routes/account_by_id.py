4f031b9f19fa8c71069a278b03b27ada46340f16
Isaac Song <isaacsongs10@gmail.com>
2024-03-20 13:52:17 -0700

    initialize AccountById class with methods

--- backend/routes/account_by_id.py
+++ backend/routes/account_by_id.py
@@ -2,3 +2,16 @@ from app_setup import db
 from flask import make_response
 from flask_restful import request, Resource
 from models.accounts import Account
+
+class AccountById(Resource):
+    def get(self, user_id):
+        pass
+
+    def post(self, user_id):
+        pass
+
+    def patch(self, id):
+        pass
+
+    def delete(self, account_id, user_id):
+        pass
