fb459d9a1919bff45dcc95e4637ed9054fe61917
Isaac Song <isaacsongs10@gmail.com>
2024-03-21 12:08:03 -0700

    add comments before each method

--- backend/routes/account_by_id.py
+++ backend/routes/account_by_id.py
@@ -4,12 +4,22 @@ from flask_restful import request, Resource
 from models.accounts import Account
 
 class AccountById(Resource):
+    # Get specific account using user id (int) and account id (string)
     def get(self, user_id, account_id):
         user_account = Account.query.filter(Account.user_id == user_id, Account.id == account_id).first()
         return user_account.to_dict(rules=('-transactions', '-user'))
 
+    # Update bank account information using user id (int) and account id (string)
     def patch(self, user_id, account_id):
         pass
 
+    # Delete Specific bank account using user id (int) and account id (string)
     def delete(self, user_id, account_id):
-        pass
+        user_account = Account.query.filter(Account.user_id == user_id, Account.id == account_id).first()
+        try:
+            db.session.delete(user_account)
+            db.session.commit()
+            return {'message': 'Bank account has been deleted'}, 200
+        except Exception as e:
+            db.session.rollback()
+            return {'error': str(e)}, 400
